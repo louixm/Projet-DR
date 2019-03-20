70b99541ab55182ba318abe26d5f9cefd9dc2023
jimy <jimy@wopr>
2019-03-20 16:21:09 +0100

    implementation add, dot, mul, constructeur

--- deathrun-portal/src/deathrun/portal/Vec2.java
+++ deathrun-portal/src/deathrun/portal/Vec2.java
@@ -14,12 +14,11 @@ public class Vec2 {
     public double y;
     
     public Vec2(double x, double y) { this.x = x; this.y = y; }
-    public Vec2 add(Vec2 other) {
-        return new Vec2(this.x+other.x, this.y+other.y);
-    }
-    public Vec2 dot(double scalar) {
-        return new Vec2(scalar*this.x, scalar*this.y);
-    }
+    public Vec2 add(Vec2 other) 	{ return new Vec2(this.x+other.x, this.y+other.y); }
+    public Vec2 sub(Vec2 other)		{ return new Vec2(this.x-other.x, this.y-other.y); }
+    public Vec2 neg() 				{ return new Vec2(-this.x, -this.y); }
+    public Vec2 mul(double scalar)	{ return new Vec2(scalar*this.x, scalar*this.y); }
+    public double dot(Vec2 other)	{ return other.x*this.x + other.y*this.y; }
     
     public String toString() {
         return "Vec2("+String.valueOf(x)+", "+String.valueOf(y)+")";
