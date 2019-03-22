45aba7d6c8641628f05ec2c5fc4345967810df5f
jimy <jimy@wopr>
2019-03-22 19:32:06 +0100

    methode de pas de simulation terminee

--- deathrun-portal/src/deathrun/portal/Game.java
+++ deathrun-portal/src/deathrun/portal/Game.java
@@ -19,7 +19,7 @@ public class Game {
     
     Sync sync;
     
-    float prev_time; // instant de dernier pas physique
+    Instant prev_time; // instant de dernier pas physique
     
     Game() {
         try {
@@ -28,29 +28,48 @@ public class Game {
         catch (SQLException e) {
             System.out.println("sql connection error, fail to init game");
         }
+        
+        prev_time = Instant.now();
     }
     
     void physicStep() {
-        float ac_time;
-        float dt = ac_time - prev_time;
+        Instant ac_time = Instant.now();
+        float dt = (ac_time.getNano() - prev_time.getNano())/1000000000;
+        
+        // simulation de mecanique des objets (rectangles) avec Euler directe
         // suppose que les données sont synchronisées et que l'etat précédent est ok
+        
         for (Player player: players) {
+            // pas de mise a jour si pas de mise a jour de vitesse
+            if (player.velocity.isnull())       continue;
             Vec2 newpos = player.position.add(player.velocity.mul(dt));
+            
             // collisions
             for (PObject object: map.objects) {
                 if (player.collisionable(object)) {
-                    // ramener la boite du joueur dans le repere 
                     Box bplayer = player.collisionBox();
                     Box bobject = object.collisionBox();
-                    Vec2 d = bplayer.distance(bobject);
+                    if (bplayer.intersect(bobject)) {
+                        // corrige la position pour que player ne soit plus dans object
+                        newpos = bobject.outer(newpos);
+                        // supprime l'acceleration dans la direction du contact
+                        Box intersection = bplayer.intersection(bobject);
+                        if      (player.acceleration.y < 0 && intersection.p2.y == bplayer.p1.y)        player.acceleration.y = 0;
+                        else if (player.acceleration.y > 0 && intersection.p1.y == bplayer.p2.y)        player.acceleration.y = 0;
+                        else if (player.acceleration.x < 0 && intersection.p2.x == bplayer.p1.x)        player.acceleration.x = 0;
+                        else if (player.acceleration.x > 0 && intersection.p1.x == bplayer.p2.x)        player.acceleration.x = 0;
+                    }
                 }
             }
+            // position maintenant corrigée
+            player.position = newpos;
+            
+            // si pas d'acceleration pas de mise a jour de vitesse
+            if (player.acceleration.isnull())       continue;
+            player.velocity = player.speed.add(player.acceleration.mul(dt));
         }
         
         prev_time = ac_time;
     }
     
-    void syncGet() {
-        // TODO mettre a jour les positions des joueurs et les pieges
-    }
 }
