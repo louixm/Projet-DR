b43a89bb9905a84c9dbf327f935a0f7588531391
BarkoPolo <48714330+BarkoPolo@users.noreply.github.com>
2019-03-23 14:15:48 +0100

    gui changes

--- deathrun-portal/src/deathrun/portal/Gui.java
+++ deathrun-portal/src/deathrun/portal/Gui.java
@@ -1,5 +1,6 @@
 package deathrun.portal;
 
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -7,6 +8,9 @@ import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.Timer;
 
 /*
@@ -21,14 +25,36 @@ import javax.swing.Timer;
  * @author ydejongh
  */
 public class Gui extends JFrame implements ActionListener, KeyListener {
-    public float scale = 30;	// pixel/m
-    public Game game;
+    public final float scale = 30;	// pixel/m
+    public final int WINDOW_WIDTH = 1280, WINDOW_HEIGHT = 1080;
     
-    Timer timer;
+    private JLabel jLabel1;
+    public Game game;
+    private BufferedImage buffer;
+    private Graphics2D bufferContext;
+    private Timer timer;
 
     
     Gui(Game game) {
-        this.game = game;
+        //initilalisation de la fenêtre graphique
+        this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.addKeyListener(this);
+        this.jLabel1 = new JLabel();
+        this.jLabel1.setPreferredSize(new java.awt.Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
+        this.setContentPane(this.jLabel1);
+        this.pack();
+        
+        // Creation du jeu
+        this.game = new Game();
+
+        // Creation du buffer pour l'affichage du jeu et recuperation du contexte graphique
+        this.buffer = new BufferedImage(this.jLabel1.getWidth(), this.jLabel1.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        this.jLabel1.setIcon(new ImageIcon(buffer));
+        this.bufferContext = this.buffer.createGraphics();
+
+        // Creation du Timer qui appelle this.actionPerformed() tous les 20 ms
         this.timer = new Timer(20, this);
         this.timer.start();
     }
@@ -62,18 +88,18 @@ public class Gui extends JFrame implements ActionListener, KeyListener {
 //        }
     }
     
+    @Override
     public void actionPerformed(ActionEvent e) {
         game.physicStep();
         repaint();
     }
 
-    @Override
-    protected void paintComponent(Graphics g) {
+    protected void paintComponent(Graphics2D g) {
         for (PObject object : game.map.objects) {
-            if (! object.foreground())	object.render(g, scale);
+            if (! object.foreground())	object.render(g, scale); }
         for (Player player : game.players)		
             player.render(g, scale);
         for (PObject object : game.map.objects) {
-            if (object.foreground())	object.render(g, scale);
-    }
+            if (object.foreground())	object.render(g, scale); }
+        }
 }
