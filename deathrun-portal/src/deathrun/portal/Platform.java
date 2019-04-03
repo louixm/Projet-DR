e8139a8852ad2a9404c76061f0c45f78da589c8f
tsoashi <tsoashi@gmail.com>
2019-04-03 11:57:27 +0200

    affichage plateforme à finir

--- deathrun-portal/src/deathrun/portal/Platform.java
+++ deathrun-portal/src/deathrun/portal/Platform.java
@@ -5,8 +5,14 @@
  */
 package deathrun.portal;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 /**
  *
@@ -14,17 +20,19 @@ import java.awt.Graphics2D;
  */
 public class Platform extends PObject {
     Box collision_box;
-
+    Image img;
+    
     public Platform(int db_id, Vec2 position) {
         super(db_id);
         this.collision_box = new Box(-2, -0.2, 2, 0);
         setPosition(position);
     }
     
-    public Platform(int db_id, Vec2 position, double width, double height) {
+    public Platform(int db_id, Vec2 position, double width, double height) throws IOException {
         super(db_id);
         this.collision_box = new Box(0, 0, width, height);
         setPosition(position);
+        img = ImageIO.read(new File("./deathrun.portal/images/plateforme.png"));
     }
     
     @Override
@@ -42,7 +50,9 @@ public class Platform extends PObject {
     
     //--------------- interface d'affichage -----------------
     public void render(Graphics2D g, float scale) {
-        super.render(g, scale);
+        Box collision_box = getCollisionBox();
+        g.drawRect(img,); // A finir
+        
         // TODO
     }
     
