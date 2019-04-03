9e132d42623f736717ba1750aa3535d7db4243cf
ydejongh <ydejongh@ELEC14.ens2m.fr>
2019-04-03 13:52:44 +0200

    toutes exceptions non gérées en interne declenchent l'arret du jeu

--- deathrun-portal/src/deathrun/portal/DeathrunPortal.java
+++ deathrun-portal/src/deathrun/portal/DeathrunPortal.java
@@ -21,16 +21,23 @@ public class DeathrunPortal {
      */
     public static void main(String[] args) throws SQLException {
         Game game = new Game();
-        game.init();	// connecte au serveur et construit tous les objets tels que dans la base de donnnées
-        Player controled = new Player("myname", 0, 0);	// TODO: choisir le nom et l'avatar du joueur
-        
-        game.players.add(controled);
-        
-        game.map.objects.add(new Platform(1, new Vec2(10, 5)));
-        game.map.objects.add(new Platform(2, new Vec2(17, 6.5), 10, 3));
-        game.map.objects.add(new Platform(3, new Vec2(3, 6.5), 1, 10));
-        game.map.objects.add(new Platform(4, new Vec2(3, 16.5), 5, 1));
-        controled.setPosition(new Vec2(10, 2));
+        Player controled;
+        try {
+            game.init();	// connecte au serveur et construit tous les objets tels que dans la base de donnnées
+            controled = new Player("myname", 0, 0);	// TODO: choisir le nom et l'avatar du joueur
+
+            game.players.add(controled);
+
+            game.map.objects.add(new Platform(1, new Vec2(10, 5),   2, 0.2));
+            game.map.objects.add(new Platform(2, new Vec2(17, 6.5), 10, 3));
+            game.map.objects.add(new Platform(3, new Vec2(3, 6.5), 1, 10));
+            game.map.objects.add(new Platform(4, new Vec2(3, 16.5), 5, 1));
+            controled.setPosition(new Vec2(10, 2));
+        }
+        catch (Exception err) {
+            System.out.println("error initializing the game: "+err);
+            return;
+        }
         
         Gui gui = new Gui(game, controled);
         gui.setVisible(true);
