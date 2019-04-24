bef51041a5e0ab108a6119647f6ab0e175404bab
kbenie <kandy.benie@ens2m.org>
2019-04-24 10:24:04 +0200

    Début de création d'une deuxième map dans mapInitialization

--- deathrun-portal/src/deathrun/portal/DeathrunPortal.java
+++ deathrun-portal/src/deathrun/portal/DeathrunPortal.java
@@ -32,7 +32,7 @@ public class DeathrunPortal {
             game.map.objects.add(new Platform(2, new Vec2(17, 6.5), 10, 3));
             game.map.objects.add(new Platform(3, new Vec2(3, 6.5), 1, 10));
             game.map.objects.add(new Platform(4, new Vec2(3, 16.5), 5, 1));*/
-            game.map = game.map.MapInitialization();
+            game.map = game.map.MapInitialization(5);  // choix de la map 1, 2, 3, 4, 5
             controled.setPosition(new Vec2(10, 2));
         }
         catch (Exception err) {
