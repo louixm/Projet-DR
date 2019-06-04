f8f2ff541b566f771422251b90e2f93a19ed3d3e
kbenie <kandy.benie@ens2m.org>
2019-06-04 18:07:32 +0200

    annimation acid

--- deathrun-portal/src/deathrun/portal/Acid.java
+++ deathrun-portal/src/deathrun/portal/Acid.java
@@ -24,10 +24,18 @@ public class Acid extends Trap {
     //Box collision_box;
     static Image img;
     static Image im;
+    static Image im0;
+    static Image im1;
+    static Image im2;
+    static Image im3;
     int typePlateforme;
     int step;
     double div=4;
+    double decalage = 50; // décalage en pixel entre la box de collision et l'image avec la vapeur
+    Vec2 position;
     static final String db_type = "acid";
+    long time_next_image;
+    long image_duration = 20000000;
     
     
     public Acid(Game game, Vec2 position) throws IOException, SQLException {
@@ -35,13 +43,16 @@ public class Acid extends Trap {
         if (img == null) {
             img = ImageIO.read(new File("./images/Acid_1.png")); //Acid
         }
-        
+        this.position = position;
         //div = 3;
         this.collision_box = new Box(0, 0, img.getWidth(null)/(div*36f), img.getHeight(null)/(div*36f)).translate(position);
         setPosition(position);
         
         im = ImageIO.read(new File("./images/Tile_10.png"));
-    
+        im0 = ImageIO.read(new File("./images/Acid_0.png"));
+        im1 = ImageIO.read(new File("./images/Acid_1.png"));
+        im2 = ImageIO.read(new File("./images/Acid_2.png"));
+        im3 = ImageIO.read(new File("./images/Acid_3.png"));
     }
     
     //--------------- interface de gestion des collisions -----------------
@@ -76,11 +87,57 @@ public class Acid extends Trap {
     @Override
     public void render(Graphics2D canvas, float scale) {
         
-        
+          // -----------------MAJ de l'image---------------------------
+            
+        long ac_time = System.nanoTime();
+        if (ac_time > time_next_image)  {    
+            
+            // si le piège n'est pas activé on met l'image "punch0"
+            if (!enabled){   
+                img = im0;
+                step=0;
+            } 
+            
+            // si le piège est activé
+            else {  
+                
+                // incrementer le compteur de frame
+                step ++;
+                
+                // évolution du numéro de l'immage
+                int fourMulticator = (int)step/4;
+                int numImage;
+                if (fourMulticator % 2 == 0){
+                    numImage = step-(4*fourMulticator);
+                }else{
+                    numImage = 3-(step-(4*fourMulticator));
+                }
+                if (numImage==0){
+                    img = im0;
+                }
+                if (numImage==1){
+                    img = im1;
+                }
+                if (numImage==2){
+                    img = im2;
+                }
+                if (numImage==3){
+                    img = im3;
+                }
+                
+            }
+            
+            
+            
+            // Redimensionnement de la box de collision
+            collision_box = new Box(0,0 , (img.getWidth(null)-decalage)/scale,(img.getHeight(null)-decalage)/scale); //la dimension de la box est celle de l'image sans vapeur
+            collision_box = collision_box.translateToPosition(position);
+            }
+            time_next_image = ac_time + image_duration;
         // ---------------affichage de PObject ----------------------      
         canvas.drawImage(img, 
                     (int) (collision_box.p1.x*scale), 
-                    (int) (collision_box.p1.y*scale), 
+                    (int) ((collision_box.p1.y*scale)-decalage), 
                     (int) (collision_box.p2.x*scale),   
                     (int) (collision_box.p2.y*scale),  
                     0, 0,
