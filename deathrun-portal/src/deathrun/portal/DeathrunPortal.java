78433ff357d970b93f533e4929cc4312e6922c91
tsoashi <tsoashi@gmail.com>
2019-06-05 10:58:47 +0200

    mur

--- deathrun-portal/src/deathrun/portal/DeathrunPortal.java
+++ deathrun-portal/src/deathrun/portal/DeathrunPortal.java
@@ -37,19 +37,21 @@ public class DeathrunPortal {
             controled.setControled(true);
 
             controled.setPosition(game.map.enter.position.add(new Vec2((game.map.enter.box.getWidth() - controled.collision_box.getWidth())/2, game.map.enter.box.getHeight() ))); //- controled.collision_box.getHeight()
-            //portails
-            Portal port = new Portal(game,new Vec2(13, 1), new boolean[] {false,true});
-            Portal port2 = new Portal(game,new Vec2(13,16), new boolean[] {false,true});
-            port.otherPortal = port2;
-            port2.otherPortal = port;
-            game.map.objects.add(port);
-            game.map.objects.add(port2);
-            //pieges
-            game.map.objects.add(new Saw(game, game.map.size.center().sub(new Vec2(1, 1))));
-            game.map.objects.add(new Laser(game,new Vec2(0, 11),0));
-            game.map.objects.add(new Punch(game,2,new Vec2(21,16)));
-            game.map.objects.add(new Acid(game, new Vec2(6,13)));
             
+//            //portails
+//            Portal port = new Portal(game,new Vec2(13, 1), new boolean[] {false,true});
+//            Portal port2 = new Portal(game,new Vec2(13,16), new boolean[] {false,true});
+//            port.otherPortal = port2;
+//            port2.otherPortal = port;
+//            game.map.objects.add(port);
+//            game.map.objects.add(port2);
+            
+//            //pieges
+//            game.map.objects.add(new Saw(game, game.map.size.center().sub(new Vec2(1, 1))));
+//            game.map.objects.add(new Laser(game,new Vec2(0, 11),0));
+//            game.map.objects.add(new Punch(game,2,new Vec2(21,16)));
+//            game.map.objects.add(new Acid(game, new Vec2(6,13)));
+//            
             controled.acceleration.y = -1; // valeur différente de 0 pour forcer l'update de physicstep initiale
             //TODO: faire plus prore que ca
             
