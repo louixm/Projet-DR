085e854a61c1840abcbec74565a9d697a506f8cf
kbenie <kandy.benie@ens2m.org>
2019-06-06 13:03:36 +0200

    Merge branch 'master' of https://github.com/louixm/Projet-DR
    # Conflicts:
    #	deathrun-portal/src/deathrun/portal/DeathrunPortal.java

--- deathrun-portal/src/deathrun/portal/Acid.java
+++ deathrun-portal/src/deathrun/portal/Acid.java
@@ -34,12 +34,13 @@ public class Acid extends PObject {
     static Image im3;
     int typePlateforme;
     int step;
+    Vec2 position;
     double div=4;
     double decalage = 50; // décalage en pixel entre la box de collision et l'image avec la vapeur
     //Vec2 position;
     static final String db_type = "acid";
     long time_next_image;
-    long image_duration = 35350000;
+    long image_duration = 35000000;
     
     
     public Acid(Game game, Vec2 position) throws IOException, SQLException {
@@ -47,14 +48,15 @@ public class Acid extends PObject {
         if (img == null) {
             img = ImageIO.read(new File("./images/Acid_i.png")); //Acid
         }
-        this.collision_box = new Box(0, 0, img.getWidth(null)/(div*36f), img.getHeight(null)/(div*36f)).translateToPosition(position);
-        //setPosition(position);
-        
+        this.position = position;
         im = ImageIO.read(new File("./images/Tile_10.png"));
         im0 = ImageIO.read(new File("./images/Acid_0.png"));
         im1 = ImageIO.read(new File("./images/Acid_1.png"));
         im2 = ImageIO.read(new File("./images/Acid_2.png"));
         im3 = ImageIO.read(new File("./images/Acid_3.png"));
+        
+        Vec2 pos = new Vec2 (position.x+im.getHeight(null)/(div*36f),position.y);
+        this.collision_box = new Box(0, 0, im.getWidth(null)/(div*36f), im.getWidth(null)/(div*36f)).translateToPosition(pos);
         
         Vec2 positionG = position;
         Vec2 positionM1 = new Vec2(position.x,position.y+im.getWidth(null)/(div*36f));
@@ -65,11 +67,14 @@ public class Acid extends PObject {
         this.platform_m1 = new Platform(game,positionM1, im.getWidth(null)/(div*36f), im.getHeight(null)/(div*36f),7);
         this.platform_m2 = new Platform(game,positionM2, im.getWidth(null)/(div*36f), im.getHeight(null)/(div*36f),7);
         this.platform_d = new Platform(game,positionD, im.getHeight(null)/(div*36f), im.getWidth(null)/(div*36f),8);
+        
+        long ac_time = System.nanoTime();
+        time_next_image = ac_time + image_duration;
     }
     
     //--------------- interface de gestion des collisions -----------------
     public int collisionable(PObject other)  { 
-        return (other instanceof Player)?2:0;
+        return (other instanceof Player)?1:0;
     }
     //--------------- interface de gestion des collisions -----------------*/
     
@@ -84,11 +89,9 @@ public class Acid extends PObject {
             Vec2 p2 = p.getCollisionBox().p2 ; //point inférieur droit
             Vec2 p3 = new Vec2(p1.x, p2.y) ; // point inférieur gauche
             Vec2 p4 = new Vec2(p2.x , p1.y) ; // point supérieur droit
-            if ((p3.x<(platform_d.position.x /(div*36f)) && p2.x>(platform_g.position.x + im.getHeight(null)/(div*36f))) && p2.y == platform_g.position.y){
+            if ((p3.x<(platform_d.position.x) && p2.x>(platform_g.position.x + im.getHeight(null)/(div*36f))) && p2.y == platform_g.position.y){
                 p.setDead(true);
                     // Player Killed
-            }else{
-                p.setDead(false);
             }
         }
     }
@@ -134,10 +137,10 @@ public class Acid extends PObject {
         time_next_image = ac_time + image_duration;
         // ---------------affichage de PObject ----------------------      
         canvas.drawImage(img, 
-                    (int) (collision_box.p1.x*scale), 
-                    (int) ((collision_box.p1.y*scale)-(decalage/div)), 
-                    (int) (collision_box.p2.x*scale),   
-                    (int) (collision_box.p2.y*scale),  
+                    (int) (position.x*scale), 
+                    (int) ((position.y*scale)-(decalage/div)), 
+                    (int) (position.x*scale + im0.getWidth(null)/div),   
+                    (int) (position.y*scale + im0.getHeight(null)/div),  
                     0, 0,
                     img.getWidth(null), img.getHeight(null),
                     null);
