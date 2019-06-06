d1fe28e87bb4ee72993e68cace152a861b94a6e9
ydejongh <ydejongh@ELEC14.ens2m.fr>
2019-06-06 13:27:21 +0200

    correction d'irregularité de temps de rafraichissement de l'acide

--- deathrun-portal/src/deathrun/portal/Acid.java
+++ deathrun-portal/src/deathrun/portal/Acid.java
@@ -27,20 +27,19 @@ public class Acid extends PObject {
     Platform platform_m2;
     Platform platform_d;
     static Image img;
-    static Image im;
-    static Image im0;
-    static Image im1;
-    static Image im2;
-    static Image im3;
+    static Image im[];
     int typePlateforme;
+    
     int step;
+    int increment;
+    
     Vec2 position;
     double div=4;
     double decalage = 50; // décalage en pixel entre la box de collision et l'image avec la vapeur
     //Vec2 position;
     static final String db_type = "acid";
     long time_next_image;
-    long image_duration = 35000000;
+    long image_duration = 100000000;
     
     
     public Acid(Game game, Vec2 position) throws IOException, SQLException {
@@ -49,27 +48,32 @@ public class Acid extends PObject {
             img = ImageIO.read(new File("./images/Acid_i.png")); //Acid
         }
         this.position = position;
-        im = ImageIO.read(new File("./images/Tile_10.png"));
-        im0 = ImageIO.read(new File("./images/Acid_0.png"));
-        im1 = ImageIO.read(new File("./images/Acid_1.png"));
-        im2 = ImageIO.read(new File("./images/Acid_2.png"));
-        im3 = ImageIO.read(new File("./images/Acid_3.png"));
+        im = new Image[] {
+            ImageIO.read(new File("./images/Tile_10.png")),
+            ImageIO.read(new File("./images/Acid_0.png")),
+            ImageIO.read(new File("./images/Acid_1.png")),
+            ImageIO.read(new File("./images/Acid_2.png")),
+            ImageIO.read(new File("./images/Acid_3.png"))
+        };
         
-        Vec2 pos = new Vec2 (position.x+im.getHeight(null)/(div*36f),position.y);
-        this.collision_box = new Box(0, 0, im.getWidth(null)/(div*36f), im.getWidth(null)/(div*36f)).translateToPosition(pos);
+        Vec2 pos = new Vec2 (position.x+im[0].getHeight(null)/(div*36f),position.y);
+        this.collision_box = new Box(0, 0, im[0].getWidth(null)/(div*36f), im[0].getWidth(null)/(div*36f)).translateToPosition(pos);
         
         Vec2 positionG = position;
-        Vec2 positionM1 = new Vec2(position.x,position.y+im.getWidth(null)/(div*36f));
-        Vec2 positionM2 = new Vec2(position.x+im.getWidth(null),position.y+im.getWidth(null)/(div*36f));
-        Vec2 positionD = new Vec2(position.x+(im.getWidth(null)+im.getHeight(null))/(div*36f),position.y);
+        Vec2 positionM1 = new Vec2(position.x,position.y+im[0].getWidth(null)/(div*36f));
+        Vec2 positionM2 = new Vec2(position.x+im[0].getWidth(null),position.y+im[0].getWidth(null)/(div*36f));
+        Vec2 positionD = new Vec2(position.x+(im[0].getWidth(null)+im[0].getHeight(null))/(div*36f),position.y);
         
-        this.platform_g = new Platform(game,positionG, im.getHeight(null)/(div*36f), im.getWidth(null)/(div*36f),9); // 36 == scale
-        this.platform_m1 = new Platform(game,positionM1, im.getWidth(null)/(div*36f), im.getHeight(null)/(div*36f),7);
-        this.platform_m2 = new Platform(game,positionM2, im.getWidth(null)/(div*36f), im.getHeight(null)/(div*36f),7);
-        this.platform_d = new Platform(game,positionD, im.getHeight(null)/(div*36f), im.getWidth(null)/(div*36f),8);
+        this.platform_g = new Platform(game,positionG, im[0].getHeight(null)/(div*36f), im[0].getWidth(null)/(div*36f),9); // 36 == scale
+        this.platform_m1 = new Platform(game,positionM1, im[0].getWidth(null)/(div*36f), im[0].getHeight(null)/(div*36f),7);
+        this.platform_m2 = new Platform(game,positionM2, im[0].getWidth(null)/(div*36f), im[0].getHeight(null)/(div*36f),7);
+        this.platform_d = new Platform(game,positionD, im[0].getHeight(null)/(div*36f), im[0].getWidth(null)/(div*36f),8);
         
         long ac_time = System.nanoTime();
         time_next_image = ac_time + image_duration;
+        
+        step = 1;
+        increment = 1;
     }
     
     //--------------- interface de gestion des collisions -----------------
@@ -89,7 +93,7 @@ public class Acid extends PObject {
             Vec2 p2 = p.getCollisionBox().p2 ; //point inférieur droit
             Vec2 p3 = new Vec2(p1.x, p2.y) ; // point inférieur gauche
             Vec2 p4 = new Vec2(p2.x , p1.y) ; // point supérieur droit
-            if ((p3.x<(platform_d.position.x) && p2.x>(platform_g.position.x + im.getHeight(null)/(div*36f))) && p2.y == platform_g.position.y){
+            if ((p3.x<(platform_d.position.x) && p2.x>(platform_g.position.x + im[0].getHeight(null)/(div*36f))) && p2.y == platform_g.position.y){
                 p.setDead(true);
                     // Player Killed
             }
@@ -109,38 +113,23 @@ public class Acid extends PObject {
         if (ac_time > time_next_image)  {   
                 
                 // incrementer le compteur de frame
-            step ++;
+            step += increment;
                 
                 // évolution du numéro de l'immage
-            int fourMulticator = (int)step/4;
-            int numImage;
-            if (fourMulticator % 2 == 0){
-                numImage = step-(4*fourMulticator);
-            }else{
-                numImage = 3-(step-(4*fourMulticator));
-            }
-            if (numImage==0){
-                img = im0;
-            }
-            if (numImage==1){
-                img = im1;
-            }
-            if (numImage==2){
-                img = im2;
-            }
-            if (numImage==3){
-                img = im3;
-            }
-                
+           
+            if (step == 4) { increment = -1; }
+            else if (step == 1) { increment = 1; }
+            
+            img = im[step];
+            time_next_image = ac_time + image_duration;
         } 
             
-        time_next_image = ac_time + image_duration;
         // ---------------affichage de PObject ----------------------      
         canvas.drawImage(img, 
                     (int) (position.x*scale), 
                     (int) ((position.y*scale)-(decalage/div)), 
-                    (int) (position.x*scale + im0.getWidth(null)/div),   
-                    (int) (position.y*scale + im0.getHeight(null)/div),  
+                    (int) (position.x*scale + img.getWidth(null)/div),   
+                    (int) (position.y*scale + img.getHeight(null)/div),  
                     0, 0,
                     img.getWidth(null), img.getHeight(null),
                     null);
