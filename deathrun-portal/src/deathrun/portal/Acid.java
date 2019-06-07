2ffaba6e4dc8c082a9a808ea72fb07bcdde92b45
kbenie <kandy.benie@ens2m.org>
2019-06-07 18:36:35 +0200

    Merge branch 'master' of https://github.com/louixm/Projet-DR
    # Conflicts:
    #	deathrun-portal/src/deathrun/portal/DeathrunPortal.java

--- deathrun-portal/src/deathrun/portal/Acid.java
+++ deathrun-portal/src/deathrun/portal/Acid.java
@@ -33,12 +33,11 @@ public class Acid extends PObject {
     int increment;
     
     Vec2 position;
-    double div=4;
+    double largeurImage=512;
     double decalage = 50; // décalage en pixel entre la box de collision et l'image avec la vapeur
-    //Vec2 position;
     static final String db_type = "acid";
     long time_next_image;
-    long image_duration = 100000000;
+    long image_duration = 400000000;
     
     
     public Acid(Game game, Vec2 position) throws IOException, SQLException {
@@ -46,7 +45,7 @@ public class Acid extends PObject {
         if (img == null) {
             img = ImageIO.read(new File("./images/Acid_i.png")); //Acid
         }
-        this.position = position;
+        
         im = new Image[] {
             ImageIO.read(new File("./images/Tile_10.png")),
             ImageIO.read(new File("./images/Acid_0.png")),
@@ -54,19 +53,24 @@ public class Acid extends PObject {
             ImageIO.read(new File("./images/Acid_2.png")),
             ImageIO.read(new File("./images/Acid_3.png"))
         };
+        double largeur = 1.5;
+        double hauteur = largeur*3/4;
         
-        Vec2 pos = new Vec2 (position.x+im[0].getHeight(null)/(div*36f),position.y);
-        this.collision_box = new Box(0, 0, im[0].getWidth(null)/(div*36f), im[0].getWidth(null)/(div*36f)).translateToPosition(pos);
         
-        Vec2 positionG = position;
-        Vec2 positionM1 = new Vec2(position.x,position.y+im[0].getWidth(null)/(div*36f));
-        Vec2 positionM2 = new Vec2(position.x+im[0].getWidth(null),position.y+im[0].getWidth(null)/(div*36f));
-        Vec2 positionD = new Vec2(position.x+(im[0].getWidth(null)+im[0].getHeight(null))/(div*36f),position.y);
+        this.collision_box = new Box(0, 0, largeur, largeur).translateToPosition(position);
+        Vec2 pos = new Vec2 (position.x-(collision_box.getWidth()/2),position.y);
         
-        this.platform_g = new Platform(game,positionG, im[0].getHeight(null)/(div*36f), im[0].getWidth(null)/(div*36f),9); // 36 == scale
-        this.platform_m1 = new Platform(game,positionM1, im[0].getWidth(null)/(div*36f), im[0].getHeight(null)/(div*36f),7);
-        this.platform_m2 = new Platform(game,positionM2, im[0].getWidth(null)/(div*36f), im[0].getHeight(null)/(div*36f),7);
-        this.platform_d = new Platform(game,positionD, im[0].getHeight(null)/(div*36f), im[0].getWidth(null)/(div*36f),8);
+        this.position = pos;
+        
+        Vec2 positionG = pos;
+        Vec2 positionM1 = new Vec2(pos.x,pos.y+(collision_box.getHeight()));
+        Vec2 positionM2 = new Vec2(positionM1.x+(collision_box.getWidth()),positionM1.y);
+        Vec2 positionD = new Vec2(positionM2.x+(collision_box.getWidth()/2),positionG.y);
+        
+        this.platform_g = new Platform(game,positionG, collision_box.getWidth()/2, collision_box.getHeight(),9); 
+        this.platform_m1 = new Platform(game,positionM1, collision_box.getWidth(), collision_box.getHeight()/2,7);
+        this.platform_m2 = new Platform(game,positionM2, collision_box.getWidth(), collision_box.getHeight()/2,7);
+        this.platform_d = new Platform(game,positionD, collision_box.getWidth()/2, collision_box.getHeight(),8);
         
         long ac_time = System.nanoTime();
         time_next_image = ac_time + image_duration;
@@ -77,27 +81,30 @@ public class Acid extends PObject {
     
     //--------------- interface de gestion des collisions -----------------
     public int collisionable(PObject other)  { 
-        return (other instanceof Player)?1:0;
+        return (other instanceof Player)?2:0;
     }
     //--------------- interface de gestion des collisions -----------------*/
     
     @Override
     public Box getCollisionBox()  { return collision_box; }
     
+    public void onCollision(Game g, PObject other) {
+        ((Player)other).setDead(true);
+    }
     
-    @Override
+    /*@Override
     public void onGameStep(Game game, float dt) {
         for (Player p: game.players){
             Vec2 p1 = p.getCollisionBox().p1 ; //point supérieur gauche
             Vec2 p2 = p.getCollisionBox().p2 ; //point inférieur droit
             Vec2 p3 = new Vec2(p1.x, p2.y) ; // point inférieur gauche
             Vec2 p4 = new Vec2(p2.x , p1.y) ; // point supérieur droit
-            if ((p3.x<(platform_d.position.x) && p2.x>(platform_g.position.x + im[0].getHeight(null)/(div*36f))) && p2.y == platform_g.position.y){
+            if ((p3.x<(platform_d.position.x) && p2.x>(platform_g.position.x + collision_box.getWidth()/2)) && p2.y == platform_g.position.y){
                 p.setDead(true);
                     // Player Killed
             }
         }
-    }
+    }*/
     
     
     
@@ -123,12 +130,13 @@ public class Acid extends PObject {
             time_next_image = ac_time + image_duration;
         } 
             
+        double ratio = (collision_box.getWidth()*scale)/largeurImage;
         // ---------------affichage de PObject ----------------------      
         canvas.drawImage(img, 
-                    (int) (position.x*scale), 
-                    (int) ((position.y*scale)-(decalage/div)), 
-                    (int) (position.x*scale + img.getWidth(null)/div),   
-                    (int) (position.y*scale + img.getHeight(null)/div),  
+                    (int) (platform_g.position.x*scale), 
+                    (int) ((platform_g.position.y*scale)-(decalage*ratio)), 
+                    (int) ((platform_d.position.x+collision_box.getWidth()/2)*scale),   
+                    (int) ((platform_m1.position.y+collision_box.getHeight()/2)*scale),  
                     0, 0,
                     img.getWidth(null), img.getHeight(null),
                     null);
